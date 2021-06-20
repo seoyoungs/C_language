37469226f70d1a1fce02c626fcd52db5af97b8f5
seoyoungs <76426566+seoyoungs@users.noreply.github.com>
2021-06-20 16:58:36 +0900

    Add files via upload

---
+++ c_coding/알고리즘1.cpp
@@ -0,0 +1,44 @@
+﻿// 알고리즘1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//vector를 이용하여 N*N의 이중 배열만들 수 있다.
+vector<int> solution(vector<int> arr, int divisor) //vector<vector<int>> arr 이중 벡터 만들기 
+{
+    vector<int> answer;
+    sort(arr.begin(), arr.end()); // 처음 끝 정렬해주기 왜??
+
+    for (int i = arr.size() - 1; i >= 0; --i) {
+        if (arr[i] < divisor) //자연수보다 작을때
+        {
+            break;///멈추기
+        }
+        if (arr[i] % divisor == 0) {
+            answer.insert(answer.begin(), arr[i]);
+        }
+    }
+    if (answer.size() == 0)
+    {
+        answer.push_back(-1); //divisor로 나누어 떨어지는 element가 없다면 -1 반환
+    }
+
+    return answer;
+}
+
+
+//문제 설명
+//array의 각 element 중 divisor로 나누어 떨어지는 값을 오름차순으로 정렬한 배열을 반환하는 함수, solution을 작성해주세요.
+//divisor로 나누어 떨어지는 element가 하나도 없다면 배열에 - 1을 담아 반환하세요.
+
+//제한사항
+//arr은 자연수를 담은 배열입니다.
+//정수 i, j에 대해 i ≠ j 이면 arr[i] ≠ arr[j] 입니다.
+//divisor는 자연수입니다.
+//array는 길이 1 이상인 배열입니다.
+
+//https://programmers.co.kr/learn/courses/30/lessons/12910
---
+++ c_coding/알고리즘2.cpp
@@ -0,0 +1,39 @@
+﻿// 알고리즘2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 0; // long long 도 되는데 int도 가능//근데 왜 longlong?
+    //long long 자료형은 int형 연산에서 초과되는 범위에서 사용
+    int size = a.size();
+    
+    for (int i = 0; i < size; i++)
+    {
+        answer += a[i] * b[i]; //a[0]*b[0] 형식이다
+    }
+
+    return answer;
+}
+
+
+
+
+//문제 설명
+//길이가 같은 두 1차원 정수 배열 a, b가 매개변수로 주어집니다.a와 b의 내적을 return 하도록 solution 함수를 완성해주세요.
+
+//이때, a와 b의 내적은 a[0] * b[0] + a[1] * b[1] + ... + a[n - 1] * b[n - 1] 입니다. (n은 a, b의 길이)
+
+//제한사항
+//a, b의 길이는 1 이상 1, 000 이하입니다.
+//a, b의 모든 수는 - 1, 000 이상 1, 000 이하입니다.
+
+//출력 예
+//a           	b	        result
+//[1, 2, 3, 4] [-3, -1, 0, 2]	3
+//[-1, 0, 1]   [1, 0, -1]     - 2
+
+//https://programmers.co.kr/learn/courses/30/lessons/70128?language=cpp
